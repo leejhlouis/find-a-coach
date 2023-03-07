a931ccfc5e8b1239f410c2bb6bde34b1eeb24ef4
leejhlouis <louisgustavo07@gmail.com>
2023-03-07 16:37:40 +0700

    optimize & use async components

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 import App from './App.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
-import BaseDialog from './components/ui/BaseDialog.vue';
 import router from './router';
 import store from './store/index';
+
+const BaseDialog = defineAsyncComponent(() =>
+  import('./components/ui/BaseDialog.vue')
+);
 
 createApp(App)
   .component('BaseBadge', BaseBadge)
